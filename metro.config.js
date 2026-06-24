98c428cf0eab0ce3f1fdfc4fd5d84740eaa1a21d
tayyab jamil <tayyabjamil777@gmail.com>
2026-06-24 15:36:49 +0100

    fix: use expo/metro-config for EAS Update compatibility

--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,7 @@
-const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+const { getDefaultConfig } = require('expo/metro-config');
+const { mergeConfig } = require('@react-native/metro-config');
 
-/**
- * Metro configuration
- * https://reactnative.dev/docs/metro
- *
- * @type {import('@react-native/metro-config').MetroConfig}
- */
+const expoConfig = getDefaultConfig(__dirname);
 const config = {};
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+module.exports = mergeConfig(expoConfig, config);
